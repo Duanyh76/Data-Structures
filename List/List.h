7ee49fc2396aba87f391f8beef548070abb7c474
Duan Yuhan <719808205@qq.com>
2016-06-08 19:23:36 +0800

    try

---
+++ List/List.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_H
+#define ElementType int
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Positon P, List L);
+Position Find(ElementType X, List L);
+void Delete(ELementType X, List L);
+Position FindPrevious(ElementType X, List L);
+void Insert(ElementType X, Position P, List L);
+void DeleteList(List L);
+Position Header(List L);
+Position First(List L);
+Position Advance(Position P);
+ElementType Retrieve(Positon P);
+
+#endif
---
+++ List/ListFunction.c
@@ -0,0 +1,62 @@
+#include "List.h"
+#include <stdlib.h>
+
+struct Node
+{
+    ElementType Element;
+    Position Next;
+};
+
+int IsEmpty(List L)
+{
+    return L->Next == NULL;
+}
+
+int IsLast(Position P, List L)
+{
+    return P->Next == NULL;
+}
+
+Position Find(ElementType X, List L)
+{
+    Position P;
+    while(P != NULL && P->Element != X)
+    {
+        P = P->Next;
+    }
+    return P;
+}
+
+List MakeEmpty(List L)
+{
+    L->Next == NULL;
+    return L;
+}
+
+void Delete(ElementType X,List L)
+{
+    Position P, TmpCell;
+    P = FindPrevious(X, L);
+    if(!IsLast(P, L))
+    {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
+}
+
+Position FindPrevious(ElementType X, List L)
+{   
+    Position P;
+    P = L;
+    while(P->Next != NULL && P->Next->Element != NULL)
+    {
+        P = P->Next;
+    }
+    return P;
+}
+
+void Insert(ElementType X, Position P, List L)
+{
+    Positi
+}
