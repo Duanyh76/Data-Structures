107a9196e14eeab1005d900521d84b2b902755cd
Duan Yuhan <719808205@qq.com>
2016-08-07 11:13:09 +0800

    finished delete function

---
+++ Heap/BinHeap.h
@@ -0,0 +1,19 @@
+#ifndef _BinHeap_H
+
+struct HeapStruct;
+typedef struct HeapStruct *PriorityQueue;
+typedef int Position;
+
+PriorityQueue Initialize( int MaxElements );
+void Destory( PriorityQueue H );
+void MakeEmpty( PriorityQueue H );
+void Insert( ElementType X, PriorityQueue H );
+ElementType DeleteMin( PriorityQueue H );
+ElementType FindMin( PriorityQueue H );
+int IsEmpty( PriorityQueue H );
+int IsFull( PriorityQueue H );
+void DecreaseKey( Position P, ElementType X, PriorityQueue H );
+void IncreaseKey( Position P, Elementtype X, PriorityQueue H );
+void Delete( Position P, PriorityQueue H );
+
+#endif
---
+++ Heap/BinHeapFunction.c
@@ -0,0 +1,157 @@
+#include "BinHeap.h"
+#define MinPQSize 1
+#define MinData 1
+
+struct HeapStruct 
+{
+    int Capacity;
+    int Size;
+    ElementType *Elements;
+};
+
+PriorityQueue Initialize( int MaxElements )
+{
+    PriorityQueue H;
+    if( MaxElement < MinPQSize )
+    {
+        Error( "Priority queue size is too small" );
+    }
+    H = malloc( sizeof( struct HeapStruct ) );
+    if( H == NULL )
+    {
+        FatalError( "Out of space" );
+    }
+    H->Elements = malloc( ( MaxElements + 1 ) * sizeof( ElementType ) );
+    if( H->Elements == NULL )
+    {
+        FatalError( "Out of space" );
+    }
+    H->Capacity = MaxElements;
+    H->Size = 0;
+    H->Elements[ 0 ] = MinData;
+}
+
+void Insert( ElementType X, PriorityQueue H )
+{
+    int i;
+    if( IsFull( H ) )
+    {
+        Error( "Priority queue is full" );
+        return;
+    }
+    for( i = ++H->Size; H->Element[ i / 2 ] > X; i /= 2 )
+    {
+        H->Elements[ i ] = H->Elements[ i / 2 ];
+    }
+    H->Elements[ i ] = X;
+}
+
+ElementType DeleteMin( PriorityQueue H )
+{
+    int i, Child;
+    ElementType MinElement, LastElement;
+    if( IsEmpty( H ) )
+    {
+        Error( "Priority queue is empty" );
+        return H->Elements[ 0 ]; 
+    }
+    MinElement = H->Elements[ 1 ];
+    LastElement = H->Elements[ H->Size-- ];
+    for( i = 1; i * 2 < H->Size || i * 2 == H->Size; i = Child )
+    {
+        Child = i * 2;
+        if( Child != H->Size && H->Elements[ Child + 1 ] < H->Elements[ Child ] )
+        {
+            Child++;
+        }
+        if( LastElement > H->Elements[ Child ] )
+        {
+            H->Elements[ i ] = H->Elements[ Child ];
+        }
+        else
+        {
+            break;
+        }
+    }
+    H->Elements[ i ] = LastElement;
+    return MinElement;
+}
+
+void Destory( PriorityQueue H )
+{
+    free( H->Elements );
+    free( H );
+}
+
+void MakeEmpty( PriorityQueue H )
+{
+    for( int i = 0; H->Elements[ i ] != NULL; i++ )
+    {
+        H->Elements[ i ] = NULL;
+    }
+}
+
+ElementType FindMin( PriorityQueue H )
+{
+    return H->Elements[ 1 ];
+}
+
+int IsEmpty( PriorityQueue H )
+{
+    return H->Elements[ 1 ] == NULL;
+}
+
+int IsFull( PriorityQueue H )
+{
+    return H->Size == H->CapaCity;
+}
+
+void DecreaseKey( Position P, ElementType X, PriorityQueue H )
+{
+    H->Elements[ P ] -= X;
+    ElementType Key;
+    Key = H->Elements[ P ];
+    Position Parent;
+    Parent = P / 2;
+    while( H->Elements[ Parent ] > H->Elements[ P ] && Parent != 0 )
+    {
+        H->Elements[ P ] = H->Elements[ Parent ];
+        P = Parent;
+        Parent /= 2;
+    }
+    H->Elements[ P ] = Key;
+}
+
+void IncreaseKey( Position P, ElementType X, PriorityQueue H )
+{
+    H->Elements[ P ] += X;
+    ElementType Key;
+    Key = H->Elements[ P ];
+    Position Child;
+    Child = P * 2;
+    if( H->Elements[ Child ] > H->Elements[ Child + 1 ] )
+    {
+        Child++;
+    }
+    while( H->Elements[ P ] > H->Elements[ Child ] && Child < H->Size + 1 )
+    {
+        H->Elements[ P ] = H->Elements[ Child ];
+        P = Child;
+        Child = P * 2;
+        if( H->Elements[ Child ] > H->Elements[ Child + 1 ] )
+        {
+            Child++;
+        }
+    }
+    H->Elements[ P ] = Key;
+}
+
+Delete( Position P, PriorityQueue H )
+{
+    ElementType Num, Tmp;
+    Num = H->Elements[ P ] - H->Elements[ 1 ] + 1;
+    Tmp = H->Elements[ P ];
+    DecreaseKey( P, Num, H );
+    Num = DeleteMin( H );
+    return Tmp;
+}
